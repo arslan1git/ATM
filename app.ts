318ad68c9e9754d861bdb93e6d43685f48c8a0de
arslan1git <arslanalichanna1@gmail.com>
2024-04-02 11:12:25 +0500

    update

---
+++ app.ts
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+
+let balance = 100000;
+let pinCode = 7787;
+
+let pin = await inquirer.prompt({
+  name: "code",
+  type: "number",
+  message: "Enter your Pin",
+});
+
+if (pin.code === pinCode) {
+  console.log("welcome");
+  let opretion = await inquirer.prompt({
+    name: "menu",
+    type: "list",
+    choices: ["withdraw", "check balance"],
+    message: "Choose one option",
+  });
+
+  if (opretion.menu === "withdraw") {
+    let amount = await inquirer.prompt({
+      name: "cash",
+      type: "number",
+      message: "Enter your Amount",
+    });
+    if (amount.cash < balance) {
+      balance -= amount.cash;
+      console.log(`Your current balance is ${balance}`);
+    } else {
+      console.log("insufficient balance");
+    }
+  } else if (opretion.menu === "check balance") {
+    console.log(`Your current balance is ${balance}`);
+  }
+} else {
+  console.log("incorrect pin");
+}
